086cc9093317aaa57fed318660d1c1aefe325b5c
fyrus7 <88237948+fyrus7@users.noreply.github.com>
2023-01-23 13:28:07 +0800

    Update keymap.c

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -1,222 +1,47 @@
 #include QMK_KEYBOARD_H
 #include "encoder.h"
-#include "midi.h"
 
-extern MidiDevice midi_device;
+#define _PAGE_1 0
+#define _PAGE_2 1
 
-#define _BASE 0
-#define _LTRM 1
-
-enum LAYOUT { LAYER_1 = 0, LAYER_2 };
-uint8_t last_stayed_layer = 0;
-
-const char* current_ltrm_func = '\0';
-static uint8_t current_MIDI_ccNumber = 1;
-
-enum midi_cc_keycodes_LTRM { MIDI_CC1 = SAFE_RANGE,
-                             MIDI_CC2, MIDI_CC3, MIDI_CC4, MIDI_CC5,
-			     MIDI_CC6, MIDI_CC7, MIDI_CC8, MIDI_CC9,
-                             MIDI_CC10, MIDI_CC11, MIDI_CC12, MIDI_CC13,
-			     MIDI_CC14, MIDI_CC15, MIDI_CC16, MIDI_CC17, MIDI_CC18 };
-
-
+/* Key Assign Layout */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[_BASE] = LAYOUT(
-	        MIDI_CC1, MIDI_CC2, MIDI_CC3, MIDI_CC4, MIDI_CC5,
-		MIDI_CC6, MIDI_CC7, MIDI_CC8, MIDI_CC9, TD(LAYER_1)
+	[_PAGE_1] = LAYOUT(
+  		    KC_A, KC_B, KC_C, KC_D, KC_E,
+		    KC_F, KC_G, KC_H, KC_I, KC_J,
+			TO(1)
 	),
-	[_LTRM] = LAYOUT(
-	        MIDI_CC10, MIDI_CC11, MIDI_CC12, MIDI_CC13, MIDI_CC14,
-		MIDI_CC15, MIDI_CC16, MIDI_CC17, MIDI_CC18, TD(LAYER_2)
+	[_PAGE_2] = LAYOUT(
+	        KC_1, KC_2, KC_3, KC_4, KC_5,
+		    KC_6, KC_7, KC_8, KC_9, KC_0,
+			TO(0)
 	),
 };
 
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case MIDI_CC1:
-            if (record->event.pressed) {
-                current_MIDI_ccNumber         =1;
-                current_ltrm_func             = "LR 01";
-            } else {
-            }
-            return false;
-            break;
-        case MIDI_CC2:
-            if (record->event.pressed) {
-                current_MIDI_ccNumber         = 2;
-                current_ltrm_func             = "LR 02";
-            } else {
-            }
-            return false;
-            break;
-        case MIDI_CC3:
-            if (record->event.pressed) {
-                current_MIDI_ccNumber         = 3;
-                current_ltrm_func             ="LR 03";
-            } else {
-            }
-            return false;
-            break;
-			case MIDI_CC4:
-            if (record->event.pressed) {
-                current_MIDI_ccNumber         = 4;
-                current_ltrm_func             = "LR 04";
-            } else {
-            }
-            return false;
-            break;
-			case MIDI_CC5:
-            if (record->event.pressed) {
-                current_MIDI_ccNumber         = 5;
-                current_ltrm_func             = "LR 05";
-            } else {
-            }
-            return false;
-            break;
-			case MIDI_CC6:
-            if (record->event.pressed) {
-                current_MIDI_ccNumber         = 6;
-                current_ltrm_func             = "LR 06";
-            } else {
-            }
-            return false;
-            break;
-			case MIDI_CC7:
-            if (record->event.pressed) {
-                current_MIDI_ccNumber         = 7;
-                current_ltrm_func             = "LR 07";
-            } else {
-            }
-            return false;
-            break;
-			case MIDI_CC8:
-            if (record->event.pressed) {
-                current_MIDI_ccNumber         = 8;
-                current_ltrm_func             = "LR 08";
-            } else {
-            }
-            return false;
-            break;
-			case MIDI_CC9:
-            if (record->event.pressed) {
-                current_MIDI_ccNumber         = 9;
-                current_ltrm_func             = "LR 09";
-            } else {
-            }
-            return false;
-            break;
-			case MIDI_CC10:
-            if (record->event.pressed) {
-                current_MIDI_ccNumber         = 10;
-                current_ltrm_func             = "LR 10";
-            } else {
-            }
-            return false;
-            break;
-			case MIDI_CC11:
-            if (record->event.pressed) {
-                current_MIDI_ccNumber         = 11;
-                current_ltrm_func             = "LR 11";
-            } else {
-            }
-            return false;
-            break;
-			case MIDI_CC12:
-            if (record->event.pressed) {
-                current_MIDI_ccNumber         = 12;
-                current_ltrm_func             = "LR 12";
-            } else {
-            }
-            return false;
-            break;
-			case MIDI_CC13:
-            if (record->event.pressed) {
-                current_MIDI_ccNumber         = 13;
-                current_ltrm_func             = "LR 13";
-            } else {
-            }
-            return false;
-            break;
-			case MIDI_CC14:
-            if (record->event.pressed) {
-                current_MIDI_ccNumber         = 14;
-                current_ltrm_func             = "LR 14";
-            } else {
-            }
-            return false;
-            break;
-			case MIDI_CC15:
-            if (record->event.pressed) {
-                current_MIDI_ccNumber         = 15;
-                current_ltrm_func             = "LR 15";
-            } else {
-            }
-            return false;
-            break;
-			case MIDI_CC16:
-            if (record->event.pressed) {
-                current_MIDI_ccNumber         = 16;
-                current_ltrm_func             = "LR 16";
-            } else {
-            }
-            return false;
-            break;
-			case MIDI_CC17:
-            if (record->event.pressed) {
-                current_MIDI_ccNumber         = 17;
-                current_ltrm_func             = "LR 17";
-            } else {
-            }
-            return false;
-            break;
-			case MIDI_CC18:
-            if (record->event.pressed) {
-                current_MIDI_ccNumber         = 18;
-                current_ltrm_func             = "LR 18";
-            } else {
-            }
-            return false;
-            break;
-        default:
-            break;
-    }
-    return true;
-}
-
-
-void dance_layer_BASE(qk_tap_dance_state_t *state, void *user_data) {
-    if (state->count == 1) {  // TAP ONCE
-        layer_on(_LTRM);
-    } else {  // TAP TWICE OR MORE
-        layer_on(last_stayed_layer);
-    }
-}
-void dance_layer_reset_BASE(qk_tap_dance_state_t *state, void *user_data) { layer_off(_BASE); }
-
-void dance_layer_LTRM(qk_tap_dance_state_t *state, void *user_data) {
-    if (state->count == 1) {  // TAP ONCE
-        layer_on(_BASE);
-    } else {  // TAP TWICE OR MORE
-        layer_on(_BASE);
-        last_stayed_layer = _LTRM;
-    }
-}
-void dance_layer_reset_LTRM(qk_tap_dance_state_t *state, void *user_data) { layer_off(_LTRM); }
-
-qk_tap_dance_action_t tap_dance_actions[] = {
-	[LAYER_1] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_layer_BASE, dance_layer_reset_BASE),
-    [LAYER_2] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, dance_layer_LTRM, dance_layer_reset_LTRM),
-};
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
-	if (index == 0) {
+	if (index == 0) { /* first encoder */
         if (clockwise) {
-                midi_send_cc(&midi_device, 0, current_MIDI_ccNumber, 65);
+                tap_code16(KC_VOLU);
             } else {
-                midi_send_cc(&midi_device, 0, current_MIDI_ccNumber, 63);
+                tap_code16(KC_VOLD);
 	    }
+	}
+    if (index == 1) { /* second encoder */
+            if (clockwise) {
+					tap_code16(KC_RGHT);
+				} else {
+					tap_code16(KC_LEFT);
+				}
+
 	}	
+    if (index == 2) { /* third encoder */
+            if (clockwise) {
+					tap_code16(KC_UP);
+				} else {
+					tap_code16(KC_DOWN);
+				}
+    }
   return true;
 }
 
@@ -227,29 +52,20 @@ bool oled_task_user() {
     oled_write_P(image, false);
 	switch (get_highest_layer(layer_state)) {
         case _PAGE_1:
-	    oled_set_cursor(0, 4);
+		oled_set_cursor(5, 3);
             oled_write_P(PSTR("LAYER [1] 2 "), false);
             break;
         case _PAGE_2:
-	    oled_set_cursor(0, 4);
+		oled_set_cursor(5, 3);
             oled_write_P(PSTR("LAYER  1 [2]"), false);
             break;
         default:
-	    oled_set_cursor(0, 4);
+		oled_set_cursor(5, 3);
             oled_write_ln_P(PSTR("NONE"), false);
-    }
-    switch (biton32(layer_state)) {
-        case _PAGE_1:
-        case _PAGE_2:
-            oled_set_cursor(0, 6);
-	    oled_write_P(PSTR("LTRM  "), false);
-            oled_write(current_ltrm_func, false);
-            oled_write_P(PSTR("           "), false);
-           break;
     }
 	return false;
 }
 #endif
 
-/* copy line below to compile */
+
 //qmk compile -kb shutterslap -km default
