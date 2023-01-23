28a86c28479a62ae31795f83f1684d88c9b5d79b
fyrus7 <88237948+fyrus7@users.noreply.github.com>
2023-01-23 13:22:06 +0800

    Update config.h

--- config.h
+++ config.h
@@ -3,21 +3,14 @@
 #include "config_common.h"
 
 /* key matrix size */
-#define MATRIX_ROWS 2
+#define MATRIX_ROWS 3
 #define MATRIX_COLS 5
 
-/*
- * Keyboard Matrix Assignments
- *
- * Change this to how you wired your keyboard
- * COLS: AVR pins used for columns, left to right
- * ROWS: AVR pins used for rows, top to bottom
- 
- * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
- *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
- */
-#define MATRIX_ROW_PINS { B2, B6 }
-#define MATRIX_COL_PINS { C6, D7, E6, B4, B5 }
+/* Keyboard Matrix IDHAM */
+#define MATRIX_ROW_PINS { F5, F4, B6 }
+#define MATRIX_COL_PINS { B2, B3, B1, F7, F6 }
+
+#define LAYER_STATE_8BIT
 
 #define TAPPING_TERM 300
 
@@ -25,13 +18,10 @@
 #define DIODE_DIRECTION COL2ROW
 
 /* Rotary encoders */
-#define ENCODERS_PAD_A { B1, F6, F4 }
-#define ENCODERS_PAD_B { B3, F7, F5 }
+#define ENCODERS_PAD_A { B5, E6, C6 }
+#define ENCODERS_PAD_B { B4, D7, D4 }
 #define ENCODER_RESOLUTION 4
-
-/* Midi options */
-#define MIDI_BASIC
-#define MIDI_ADVANCE
+//#define ENCODER_DIRECTION_FLIP
 
 /* Debounce reduces chatter (unintended double-presses)
  - set 0 if debouncing is not needed */
@@ -40,17 +30,16 @@
 /* disable action features */
 #define NO_ACTION_TAPPING
 #define NO_ACTION_ONESHOT
+#define NO_MUSIC_MODE
+
 
 /* Bootmagic Lite key configuration */
 #define BOOTMAGIC_LITE_ROW 0
 #define BOOTMAGIC_LITE_COLUMN 0
 
-
 /* enable oled display */
 #define OLED_DISPLAY_128X64
-#define OLED_FONT_H "keymaps/default/glcdfont.c"
-#define OLED_DISABLE_TIMEOUT
+#define OLED_FONT_H "font/glcdfont.c"
 
 
-/* copy line below to compile */
-//qmk compile -kb shutterslap -km default
+// make shutterslap:default
